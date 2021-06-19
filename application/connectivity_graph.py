3507dadc6f1074f41fef4d2a296785425b9b3d43
Thomas Krijnen <t.krijnen@gmail.com>
2021-06-19 15:25:14 +0200

    apply length unit to elevation

--- application/connectivity_graph.py
+++ application/connectivity_graph.py
@@ -33,6 +33,55 @@ if WITH_MAYAVI:
 import ifcopenshell
 import ifcopenshell.geom
 
+
+#####################
+
+prefix_mapping = {
+    "EXA"  : 1.e18,
+    "PETA" : 1.e15,
+    "TERA" : 1.e12,
+    "GIGA" : 1.e9,
+    "MEGA" : 1.e6,
+    "KILO" : 1.e3,
+    "HECTO": 1.e2,
+    "DECA" : 1.,
+    "DECI" : 1.e-1,
+    "CENTI": 1.e-2,
+    "MILLI": 1.e-3,
+    "MICRO": 1.e-6,
+    "NANO" : 1.e-9,
+    "PICO" : 1.e-12,
+    "FEMTO": 1.e-15,
+    "ATTO" : 1.e-18
+}
+
+def get_unit(f, unit_type, default=None):
+    us = [u for u in f.by_type("IfcUnitAssignment")[0][0] if hasattr(u, "UnitType") and u.UnitType == unit_type]
+    if len(us) == 0:
+        return default
+        
+    u = us[0]
+                
+    F = 1.
+    if u.is_a("IfcConversionBasedUnit"):
+        fc, u = u.ConversionFactor
+        F = fc.wrappedValue
+        
+    assert u.is_a("IfcSIUnit")
+    S = prefix_mapping.get(u.Prefix, 1.)
+
+    return F * S
+
+################
+
+def wrap_try(fn):
+    def inner(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except:
+            return None
+    return inner
+
 id, fns, command = sys.argv[1:]
 
 fns = ast.literal_eval(fns)
@@ -206,9 +255,11 @@ class ifc_element:
         
 inf = float("inf")
 
+
 fs = list(map(ifcopenshell.open, fns))
+lu = get_unit(fs[0], "LENGTHUNIT", 1.0)
 storeys = sum(map(lambda f: f.by_type("IfcBuildingStorey"), fs), [])
-elevations = list(map(lambda s: getattr(s, 'Elevation', 0.), storeys))
+elevations = list(map(lambda s: lu * getattr(s, 'Elevation', 0.), storeys))
 storey_by_elevation = dict(zip(elevations, storeys))
 elevations = sorted(set(elevations))
 elevations_2 = list(elevations)
