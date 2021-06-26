d7b8723096a5f1e31ead08597554efd086b9c652
aothms <t.krijnen@gmail.com>
2021-06-26 16:32:26 +0200

    --components and --orient options in convert.py

--- application/convert.py
+++ application/convert.py
@@ -6,6 +6,8 @@ import bpy
 assert '--' in sys.argv
 sep = sys.argv.index('--')
 split = '--split' in sys.argv
+components = '--components' in sys.argv
+orient = "--orient" in sys.argv
 
 bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete()
@@ -18,16 +20,30 @@ for fn in sys.argv[sep + 1:-1]:
     else:
         print(fn)
         exit()
+        
+if orient:
+    for obj in list(bpy.data.objects):
+        bpy.ops.object.select_all(action='DESELECT')
+        obj.select_set(True)
+        bpy.context.view_layer.objects.active = obj
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.mesh.remove_doubles()
+        bpy.ops.mesh.normals_make_consistent(inside=False)
+        bpy.ops.object.editmode_toggle()
 
 if split:
     for ob in bpy.data.objects:
         print(ob.name)
     get_id = lambda nm: nm.split('.')[0].split('-')[-1]
     for id in {get_id(ob.name) for ob in bpy.data.objects}:
-        try:
-            int(id)
-        except:
-            continue
+        if components:
+            pass
+        else:
+            try:
+                int(id)
+            except:
+                continue
 
         for ob in bpy.data.objects:
             if hasattr(ob, 'select'):
