d7567d983138fdd26c42a5e44bdcde94508c84ca
Thomas Krijnen <t.krijnen@gmail.com>
2022-05-13 11:28:50 +0200

    Upgrade to bimsurfer 2.1

--- application/static/App.js
+++ application/static/App.js
@@ -1,23 +1,33 @@
-require(["bimsurfer/src/MultiModal", "bimsurfer/lib/domReady!"], function (Viewer) {
-    var v = window.viewer = new Viewer({
-        domNode: 'right',
-        svgDomNode: 'bottom',
-        modelId: window.MODEL_ID,
-        withTreeVisibilityToggle: true,
-        withTreeViewIcons: true,
-        n_files: window.NUM_FILES
-    });
-    if (window.SPINNER_CLASS) {
-        v.setSpinner({className: window.SPINNER_CLASS});
-    } else if (window.SPINNER_URL) {
-        v.setSpinner({url: window.SPINNER_URL});
-    }
-    v.load2d();
-    v.load3d();
-    v.loadMetadata('middle');
-    v.loadTreeView('top');
-    
-    if (window.onViewerLoaded) {
-        window.onViewerLoaded(self);
-    }
+import MultiModal from './bimsurfer/src/MultiModal.js';
+
+let processed = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (processed) {
+    // For some reason the event fires twice with es-module-shims on chrome
+    return;
+  }
+  processed = true;
+
+  var v = window.viewer = new MultiModal({
+    domNode: 'right',
+    svgDomNode: 'bottom',
+    modelId: window.MODEL_ID,
+    withTreeVisibilityToggle: true,
+    withTreeViewIcons: true,
+    n_files: window.NUM_FILES,
+  });
+  if (window.SPINNER_CLASS) {
+    v.setSpinner({className: window.SPINNER_CLASS});
+  } else if (window.SPINNER_URL) {
+    v.setSpinner({url: window.SPINNER_URL});
+  }
+  v.load2d();
+  v.load3d();
+  v.loadMetadata('middle');
+  v.loadTreeView('top');
+
+  if (window.onViewerLoaded) {
+    window.onViewerLoaded(self);
+  }
 });
