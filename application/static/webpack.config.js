f7ac4276737a557cc2b491f0ffd2c079b004c336
Thomas Krijnen <t.krijnen@gmail.com>
2022-05-13 12:16:18 +0000

    Minor fixes

--- application/static/webpack.config.js
+++ application/static/webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 
 module.exports = env => ({
-   entry: './App.js',
-   resolve: {
-     alias: {
-       './bimsurfer': path.resolve(__dirname, '../bimsurfer/bimsurfer'),
-       'three': path.resolve(__dirname, '../bimsurfer/bimsurfer/lib/three/r140/three.module.js')
-     }
-   },
-   output: {
-     filename: `App.${env.postfix}.js`
-   },
- });
+  entry: './App.js',
+  resolve: {
+    alias: {
+      './bimsurfer': path.resolve(__dirname, '../bimsurfer/bimsurfer'),
+      'three': path.resolve(__dirname, '../bimsurfer/bimsurfer/lib/three/r140/three.module.js'),
+    }
+  },
+  output: {
+    filename: `App.${env.postfix}.js`,
+    path: path.resolve(__dirname, '.'),
+  },
+});
+ 
