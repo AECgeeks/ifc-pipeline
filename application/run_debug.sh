ea390a2daa6a33cf513c1fa75cfa906222719cfd
Thomas Krijnen <t.krijnen@gmail.com>
2022-06-02 10:40:44 +0200

    Don't depend on redis in live dev mode

--- application/run_debug.sh
+++ application/run_debug.sh
@@ -1,1 +1,1 @@
-with_redis=true environment=development FLASK_APP=main.py FLASK_DEBUG=1 python3 -m flask run -h 0.0.0.0 --with-threads
+environment=development FLASK_APP=main.py FLASK_DEBUG=1 python3 -m flask run -h 0.0.0.0 --with-threads
